e1615afaa3d0f96b378d8c3606c46c8f987c7534
HAdrian0205 <99258953+HAdrian0205@users.noreply.github.com>
2022-03-20 21:05:10 +0100

    Add files via upload

---
+++ UY5E1L_0308/UY5E1L1fel.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc){
+    if(argc == 1){
+        for(int i = 0; i < 10; i++){
+            system("dir");
+        }
+    }
+    system("ketto");
+    return 0;
+}
---
+++ UY5E1L_0308/UY5E1L2fel.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char input[100];
+    do{
+        scanf("%s", input);
+        system(input);
+    }while(1);
+    return 0;
+}
